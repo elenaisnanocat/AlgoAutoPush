d0a0539feecd701d8b8bfaad50d7ec4db7a3bde5
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2022-06-15 01:00:26 +0900

    [Bronze I] Title: 이항 계수 1, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,5 +1,12 @@
-from itertools import combinations
+def factorial(n):
+    ans = 1
+    for i in range(2,n+1):
+        ans*=i
+    return ans
 
-N,K = map(int,input().split())
-com = list(combinations(range(N),K))
-print(len(com))
+def combination(n,k):
+    return factorial(n)//factorial(n-k)//factorial(k)
+
+n,k = map(int,input().split())
+result = combination(n,k)
+print(result)
