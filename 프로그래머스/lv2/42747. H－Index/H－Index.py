fcc4a7042da7724f0a3f9314abfa461acf2a4d48
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2022-10-24 15:19:18 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    answer = len(citations)
+    citations.sort(reverse=True)
+    # print(citations)
+    for i , c in enumerate(citations):
+        print(i,c)
+        if i >= c:
+            return i
+            # print(i)
+    return answer
