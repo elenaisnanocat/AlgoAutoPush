bf3252efa170c02b621f47a9ac4fea1c0bbada7d
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2022-10-26 11:44:18 +0900

    [level 0] Title: 소인수분해, Time: 0.62 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = []
+    for i in range(2,n+1):
+        if n % i == 0 :
+            answer.append(i)
+        while n % i == 0:
+            n = n/i
+    return answer
