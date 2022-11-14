6b80965a1ddb8b876cc4027ea71f05ba0d2f090b
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2022-11-14 16:43:46 +0900

    Update 미로 탐색.py

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,3 +1,33 @@
+# from collections import deque
+
+# N, M = map(int,input().split())
+# arr = []
+# for _ in range(N):
+#     arr.append(list(map(int,input())))
+
+# que = deque([(0,0)])
+
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+
+# visited = [[0]*M for _ in range(N)]
+# visited[0][0] = 1
+
+# while que:
+#     x, y = que.popleft()
+#     for k in range(4):
+#         nx = x + dx[k]
+#         ny = y + dy[k]
+
+#         if 0 <= nx < N and 0 <= ny < M :
+#             if visited[nx][ny] == 0 and arr[nx][ny] == 1:
+#                 visited[nx][ny] = visited[x][y] + 1
+#                 que.append((nx,ny))
+
+# print(visited[N-1][M-1])
+# 11/14일 통과 코드
+
+
 from collections import deque
 
 N, M = map(int, input().split())
