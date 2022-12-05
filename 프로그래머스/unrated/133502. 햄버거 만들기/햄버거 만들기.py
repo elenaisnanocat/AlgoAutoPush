5583547d23673939940b751e09f15a256a3f5aa6
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2022-12-05 16:33:45 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,10 +1,3 @@
 def solution(ingredient):
     answer = 0
-    stack = []
-    for i in ingredient:
-        stack.append(i)
-        if stack[-4:] == [1,2,3,1]:
-            answer += 1
-            for j in range(4):
-                stack.pop()
     return answer
