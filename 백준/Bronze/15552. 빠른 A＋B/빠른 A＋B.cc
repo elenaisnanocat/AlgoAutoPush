90bd08669deeabef9f571a908b6786fb1ecd9097
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-09-25 02:47:27 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T;
+    cin >> T;
+    for(int t= 0; t<T; t++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+}
