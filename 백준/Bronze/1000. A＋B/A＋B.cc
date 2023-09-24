14d0b33a74a6ed7eec38fa9a1afbc7b1f4e244a1
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-09-24 19:49:06 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h>
+
+#include <iostream>
+
 using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int a, b;
-  cin >> a >> b;
-  cout << a + b;
+int main(void) {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
 }
