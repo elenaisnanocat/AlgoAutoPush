c67920befd93501078a265bc55a5937b78a13027
YuJung Park <87461688+elenaisnanocat@users.noreply.github.com>
2023-09-30 17:33:54 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    stack<int> st;
+    
+    while(n--){
+        string s;
+        cin >> s;
+        if(s == "push"){
+            int t;
+            cin >> t;
+            st.push(t);
+        }
+        else if(s == "pop"){
+            if(st.empty()) cout << -1 << "\n";
+            else{
+                cout << st.top() << "\n";
+                st.pop();
+            }
+        }
+        else if(s == "size")
+            cout << st.size() << "\n";
+        else if(s == "empty")
+            cout << (int)st.empty() << "\n";
+        else if(s == "top"){
+            if(st.empty()) cout << -1 << "\n";
+            else cout << st.top() << "\n";
+        }
+    }
+}
